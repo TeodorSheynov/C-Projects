be7e3f3437b1fb58ef304be8487ea7186a677b20
TeodorSheynov <teodor.sheynov@gmail.com>
2020-12-03 19:21:12 +0200

    C# Exercise Solutions

---
+++ Final_Exam_3/HeroesOfCodeAndLogic/Hero.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeroesOfCodeAndLogic
+{
+    class Hero
+    {
+        public string Name { get; set; }
+        public int Hp { get; set; }
+        public int Mana { get; set; }
+
+        public Hero(string name,int hp,int mana)
+        {
+            this.Name = name;
+            this.Hp = hp;
+            this.Mana = mana;
+        }
+        public void CastSpell(int manaNedded,string spellName)
+        {
+            if(Mana<manaNedded)
+            {
+                Console.WriteLine($"{Name} does not have enough MP to cast {spellName}!");
+            }else
+            {
+                int manaLeft = Mana - manaNedded;
+                Console.WriteLine($"{Name} has successfully cast {spellName} and now has {manaLeft} MP!");
+                Mana = manaLeft;
+            }
+        }
+        public bool TakeDamage(int damage,string attacker)
+        {
+            int healthleft = this.Hp - damage;
+            if (healthleft<=0)
+            {
+                Console.WriteLine($"{this.Name} has been killed by {attacker}!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"{Name} was hit for {damage} HP by {attacker} and now has {healthleft} HP left!");
+                Hp = healthleft;
+                return false;
+            }
+        }
+        public void Recharge(int amount)
+        {
+            
+            if (Mana+ amount> 200)
+            {
+                Console.WriteLine($"{Name} recharged for {200 - Mana} MP!");
+                Mana = 200;
+            }
+            else
+            {
+                Mana += amount;
+                Console.WriteLine($"{Name} recharged for {amount} MP!");
+            }
+        }
+        public void Heal(int healing)
+        {
+            
+            if (Hp +healing> 100)
+            {
+               
+                Console.WriteLine($"{Name} healed for {100-Hp} HP!");
+                Hp = 100;
+
+
+            }
+            else
+            {
+                Console.WriteLine($"{Name} healed for {healing} HP!");
+                Hp += healing;
+            }
+        }
+        public void Print()
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine($"  HP: {Hp}");
+            Console.WriteLine($"  MP: {Mana}");
+        }
+
+    }
+}
---
+++ Final_Exam_3/HeroesOfCodeAndLogic/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+
+
+
+namespace HeroesOfCodeAndLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, Hero> hero = new Dictionary<string, Hero>();
+            // adding heroes to Dictionary to store
+            for (int i = 0; i < n; i++)
+            {
+                string[] parameters = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                hero.Add(parameters[0], new Hero(parameters[0], int.Parse(parameters[1]), int.Parse(parameters[2])));
+            }
+            //reading input of commands
+            string[] comands = Console.ReadLine().Split(" - ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (comands[0] != "End")
+            {
+
+                //read the comand
+                switch (comands[0])
+                {
+                    case "Heal":
+                        hero[comands[1]].Heal(int.Parse(comands[2]));
+                        break;
+                    case "Recharge":
+                        hero[comands[1]].Recharge(int.Parse(comands[2]));
+                        break;
+                    case "TakeDamage":
+                        if (hero[comands[1]].TakeDamage(int.Parse(comands[2]), comands[3]))
+                        {
+                            hero.Remove(comands[1]);
+                        }
+                        break;
+                    case "CastSpell":
+                        hero[comands[1]].CastSpell(int.Parse(comands[2]), comands[3]);
+                        break;
+                }
+             //read next comand
+             comands = Console.ReadLine().Split(" - ", StringSplitOptions.RemoveEmptyEntries);
+            }
+            foreach (var item in hero.OrderByDescending(x=>x.Value.Hp).ThenBy(x=>x.Value.Name))
+            {
+                item.Value.Print();
+            }
+        }
+    }
+}
+
