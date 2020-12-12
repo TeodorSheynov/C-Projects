7c3eeb7a6d159e266df2981b10dc0a71fafa6b6f
Teodor <74595485+TeodorSheynov@users.noreply.github.com>
2020-12-12 16:32:28 +0200

    C# Final Exam

---
+++ Final_Exam_Preperation/Exam_Password_Reset/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exam_Password_Reset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            string result = string.Empty;
+            StringBuilder builder = new StringBuilder();
+            string[] command = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            List<string> oddComand = new List<string>();
+            
+
+            while (command[0]!="Done")
+            {
+                switch (command[0].ToLower())
+                {
+                    case "takeodd":
+                        for (int i = 0; i < password.Length; i++)
+                        {
+                            if (i%2!=0)
+                            {
+                                builder.Append(password[i]);
+                            }
+                        }
+                        Console.WriteLine(builder.ToString());
+                        password = builder.ToString();
+                        builder.Clear();
+                        break;
+                    case "cut":
+                        oddComand = password.ToCharArray().Select(x => x.ToString()).ToList<string>();
+                        oddComand.RemoveRange(int.Parse(command[1]), int.Parse(command[2]));
+                        foreach (var item in oddComand)
+                        {
+                            builder.Append(item);
+                        }
+                        Console.WriteLine(builder.ToString());
+                        password = builder.ToString();
+                        builder.Clear();
+                        oddComand.Clear();
+                        break;
+                    case "substitute":
+                        if (password.Contains(command[1]))
+                        {
+                            while (password.Contains(command[1]))
+                            {
+                                password = password.Replace(command[1], command[2]);
+                            }
+                            Console.WriteLine(password);
+                        }else
+                        {
+                            Console.WriteLine("Nothing to replace!");
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                command= Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            }
+            Console.WriteLine($"Your password is: {password}");
+        }
+    }
+}
---
+++ Final_Exam_Preperation/FancyBarcodes_solution/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FancyBarcodes_solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string barcodePattern= @"(?:@#+)([A-Z][A-Za-z0-9]{4,}[A-Z])(?:@#+)";
+            string numberPattern = @"\d";
+            StringBuilder numberBarcode = new StringBuilder();
+            
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                string barcode = Console.ReadLine();
+                Regex regexBarcode = new Regex(barcodePattern);
+                Regex regexNumbers = new Regex(numberPattern);
+                MatchCollection numberMatches= regexNumbers.Matches(barcode);
+
+                if (regexBarcode.IsMatch(barcode))
+                {
+                    if (regexNumbers.IsMatch(barcode))
+                    {
+                        foreach (Match item in numberMatches)
+                        {
+                            numberBarcode.Append(item.Value);
+                        }
+                        Console.WriteLine($"Product group: {numberBarcode}");
+                        numberBarcode.Clear();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Product group: 00");
+                    }
+                }else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+                    
+            }
+           
+        }
+    }
+}
